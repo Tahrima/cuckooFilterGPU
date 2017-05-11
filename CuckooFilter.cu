ae23b76be28656ecc06faf480ad7ede526e0b3b9
Timothy <tsalmon2000@gmail.com>
2017-05-10 23:51:30 -0400

    Fixed up cuckoo filter class

--- CuckooFilter.cu
+++ CuckooFilter.cu
@@ -2,8 +2,10 @@ class CuckooFilter {
   public:
     char** buckets;
     unsigned int numBuckets;
-    __device__ __host__ CuckoFilter(unsigned int numberOfBuckets, unsigned int bucketSize) {
+    unsigned int bucketSize;
+    __device__ __host__ CuckoFilter(unsigned int numberOfBuckets, unsigned int bucketSizeIn) {
       numBuckets = numberOfBuckets;
+      bucketSize = bucketSizeIn;
       cudaMalloc((void**)&buckets, sizeof(char*) * numBuckets);
       for(int i=0; i<numBuckets; i++){
         cudaMalloc((void**)&buckets[i], sizeof(char) * bucketSize);
@@ -20,6 +22,13 @@ class CuckooFilter {
     }
     __device__ unsigned int lookup(unsigned int bucketNum, unsigned int index) {
       return(buckets[bucketNum][index]);
+    }
+    __device__ unsigned int lookupFingerprintInBucket(unsigned int fingerprint, unsigned int bucketNum) {
+      int retVal = 0;
+      for (int i = 0; i < bucketSize; i++) {
+        retVal = retVal || (fingerprint == buckets[bucketNum][i]);
+      }
+      return(retVal);
     }
 
     __device__ void printFilter() {
